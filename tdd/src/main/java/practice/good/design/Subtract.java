59390c4ec50c98d230429dea964b4bf71a4d18b5
zachcannon <zach.cannon10@gmail.com>
2015-10-08 11:04:59 -0500

    Created subtraction operand and added test for it

---
+++ tdd/src/main/java/practice/good/design/Subtract.java
@@ -0,0 +1,16 @@
+package practice.good.design;
+
+/**
+ * Created by zacharycannon on 10/8/15.
+ */
+public class Subtract extends Operand {
+
+    public Subtract() {
+        super("-");
+    }
+
+    @Override
+    public Integer evaluate(int input1, int input2) {
+        return Math.subtractExact(input1, input2);
+    }
+}
