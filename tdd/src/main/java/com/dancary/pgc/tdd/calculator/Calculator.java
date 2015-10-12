305cbdd81bd917a543ab675eae72ffced9d281e2
Dan <cary.dan@gmail.com>
2015-10-12 11:34:08 -0500

    Working on making tests pass

--- tdd/src/main/java/com/dancary/pgc/tdd/calculator/Calculator.java
+++ tdd/src/main/java/com/dancary/pgc/tdd/calculator/Calculator.java
@@ -1,21 +1,28 @@
 package com.dancary.pgc.tdd.calculator;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by caryd on 9/28/2015.
  */
 public class Calculator {
-
+    private Double[] numbersToAdd;
 
     public void push(double value) {
-
+        int i = numbersToAdd.length;
+        i += i+1;
+        numbersToAdd[i] = value;
     }
 
-    public void add(){
-
+    public double add(){
+        Double total = 0.0;
+        for (Double number : this.numberToAdd){
+            total += number;
+        }
+        return this.value(total);
     }
 
-    public double value(){
-
-        return 0;
+    public double value(Double value){
+        return value;
     }
 }
