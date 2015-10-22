656f9bb821c84c868e5e2ce808da1c011a35c930
mikeabney <github@mikeabney.com>
2015-10-22 16:35:31 -0500

    Starting to print scoresheet: Header and border rows.

---
+++ object-calisthenics/src/test/java/com/mikeabney/pgc/bowling/com/mikeabney/pgc/bowling/console/FrameHeaderPrinterTest.java
@@ -0,0 +1,19 @@
+package com.mikeabney.pgc.bowling.com.mikeabney.pgc.bowling.console;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class FrameHeaderPrinterTest {
+    @Test
+    public void shouldPrintAppropriateBorder() {
+        int maxNameLength = 10;
+        FrameHeaderPrinter printer = new FrameHeaderPrinter(maxNameLength);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        printer.print(writer);
+        Assert.assertEquals("|            |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   10    |\n", stringWriter.toString());
+    }
+}
--- object-calisthenics/src/test/java/com/mikeabney/pgc/bowling/console.java
+++
@@ -1,7 +0,0 @@
-package com.mikeabney.pgc.bowling;
-
-/**
- * Created by mike on 10/22/15.
- */
-public class console {
-}
